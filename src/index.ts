1244d405e5c70d1783c1c93883ed904091cea68d
Jack Matthews <jm5112356@gmail.com>
2022-05-07 01:29:00 -0400

    Use spread syntax

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,7 @@ import { Plugin, registerPlugin } from "enmity-api/plugins";
 import Manifest from "./manifest.json";
 
 const ExamplePlugin: Plugin = {
-  name: Manifest.name,
-  // @ts-ignore
-  description: Manifest.description,
-  authors: Manifest.authors,
-  version: Manifest.version,
-  color: Manifest.color,
+  ...Manifest,
   commands: [],
   patches: [],
 
